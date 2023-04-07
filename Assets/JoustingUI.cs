805f0f3c864510ef86715c15e186fd36b2ff2f93
dimos222 <dimos222@yahoo.gr>
2023-04-07 11:34:00 +0300

    Add unity relay

--- Assets/JoustingUI.cs
+++ Assets/JoustingUI.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,12 +6,26 @@ using UnityEngine.UI;
 public class JoustingUI : MonoBehaviour
 {
     [SerializeField] private Button hostButton;
+    [SerializeField] private Button joinButton;
+    [SerializeField] private TMP_InputField joinCodeField;
+    [SerializeField] private GameObject networkManager;
+
 
     private void Awake()
     {
-        hostButton.onClick.AddListener(() =>
+        RelayManager relayManager = networkManager.GetComponent<RelayManager>();
+
+        hostButton.onClick.AddListener(async () =>
         {
-            NetworkManager.Singleton.StartHost();
+            string joinCode = await relayManager.CreateRelay();
+            joinCodeField.text = joinCode;
+            joinCodeField.DeactivateInputField();
+        });
+
+        joinButton.onClick.AddListener(() =>
+        {
+            string joinCode = joinCodeField.text;
+            relayManager.JoinRelay(joinCode);
         });
     }
 }
